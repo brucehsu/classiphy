122da275ab28c67063bf67868ced29f7f454917f
Bruce Hsu <brucehsu@gmail.com>
2010-12-07 11:12:01 +0800

    Fix: Navigation keys doesn't work on photo list.

--- nosearchlist.cpp
+++ nosearchlist.cpp
@@ -11,7 +11,9 @@ void NoSearchList::keyboardSearch(const QString &search) {
 }
 
 void NoSearchList::keyPressEvent(QKeyEvent *event) {
-    if(event->key()==Qt::Key_Down||event->key()==Qt::Key_Up) {
+    if(event->key()==Qt::Key_Down||event->key()==Qt::Key_Up
+       ||event->key()==Qt::Key_PageUp||event->key()==Qt::Key_PageDown
+       ||event->key()==Qt::Key_Home||event->key()==Qt::Key_End) {
         QListWidget::keyPressEvent(event);
         return;
     }
