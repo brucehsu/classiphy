01cade75013bce2b9956b342833d458288650289
Bruce Hsu <brucehsu@gmail.com>
2010-12-09 14:58:53 +0800

    Change: Some application information modification.

--- const.h
+++ const.h
@@ -1,8 +1,8 @@
 #include <QString>
 #ifndef CONST_H
 #define CONST_H
-    const QString version = "0.2 beta";
-    const QString website = "http://brucehsu.googlepages.com/classiphy";
+    const QString version = "0.2.0";
+    const QString website = "https://github.com/brucehsu/classiphy/wiki";
     const QString authurs = "Bruce Hsu";
     const int ZipMode=1315,FolderMode=1316;
 #endif // CONST_H
