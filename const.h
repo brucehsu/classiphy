0f54dc92d916f4299cadc2a900d1590575b077a4
Bruce Hsu <brucehsu@gmail.com>
2010-12-16 23:10:50 +0800

    Change: Version changed to 0.3.0

--- const.h
+++ const.h
@@ -1,7 +1,7 @@
 #include <QString>
 #ifndef CONST_H
 #define CONST_H
-    const QString version = "0.2.0";
+    const QString version = "0.3.0";
     const QString website = "https://github.com/brucehsu/classiphy/wiki";
     const QString authurs = "Bruce Hsu";
     const int ZipMode=1315,FolderMode=1316;
